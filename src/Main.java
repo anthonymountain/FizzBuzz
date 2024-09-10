a24551700fd9c064e6f029c2c3e5fd247bf30024
askk7852 <shianthonyshan@gmail.com>
2024-09-09 20:27:44 -0400

    in Main.java, call FizzBuzz.main with String[] nuts ticked more boxes in README.md

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import com.sun.jdi.connect.Connector;
+
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzBuzz(30);
+//        FizzBuzz.doFizzBuzz(30);
+        String[] p = new String[] {};
+        FizzBuzz.main(p);
     }
 }
